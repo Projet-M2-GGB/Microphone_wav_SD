ddad394eb22270654930f98afce01648c873dd04
Tresnov2142 <147144536+Tresnov2142@users.noreply.github.com>
2025-01-09 11:11:30 +0100

    heap stack upgrade
    .

--- X-CUBE-AI/App/network_data_params.c
+++ X-CUBE-AI/App/network_data_params.c
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    network_data_params.c
   * @author  AST Embedded Analytics Research Platform
-  * @date    2025-01-09T10:38:02+0100
+  * @date    2025-01-09T11:04:21+0100
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * Copyright (c) 2025 STMicroelectronics.
